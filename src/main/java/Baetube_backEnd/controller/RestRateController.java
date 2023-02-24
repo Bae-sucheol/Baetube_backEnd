767af40d221ed896307c1753c754dd53098b1a2a
Bae-sucheol <81005707+Bae-sucheol@users.noreply.github.com>
2023-02-24 21:23:27 +0900

    2023/02/24
    1. imageView를 위에 덮는 형식으로 변경하니 잘 작동한다.
    2. 하지만 클릭이 안되는 현상이 발생한다. bottomsheet 와 겹쳐서 발생하는 문제인지 뭔지 모르겠으나 해결이 안된다. 오늘은 여기까지.

--- src/main/java/Baetube_backEnd/controller/RestRateController.java
+++ src/main/java/Baetube_backEnd/controller/RestRateController.java
@@ -6,22 +6,17 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import Baetube_backEnd.ErrorResponse;
 import Baetube_backEnd.dto.Rate;
-import Baetube_backEnd.dto.Reply;
 import Baetube_backEnd.exception.NullRateResultException;
-import Baetube_backEnd.exception.WrongIdPasswordException;
 import Baetube_backEnd.service.rate.RateService;
 
 @RestController
